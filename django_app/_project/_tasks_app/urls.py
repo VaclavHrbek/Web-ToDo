04d016f8a6aeee3c402de2b882760ddab7fb571b
VaclavHrbek <st43190@student.upce.cz>
2024-11-06 18:32:44 +0100

    Add new basic feautres.

--- django_app/_project/_tasks_app/urls.py
+++ django_app/_project/_tasks_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "_tasks_app"
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('tasks/', views.tasks, name='tasks'),
-    path('tasks/<int:task_id>/', views.task, name='task')
+    path("", views.index, name='index'),
+    path("tasks/", views.tasks, name='tasks'),
+    path("tasks/<int:task_id>/", views.task, name='task')
 ]
--- django_app/_project/_tasks_app/views.py
+++ django_app/_project/_tasks_app/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+
+from .models import Task
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    latest_tasks_list = Task.objects.order_by('-created_at')[:5]
+    context = {"latest_tasks_list": latest_tasks_list}
+    return render(request, 'index.html', context)
 
 def tasks(request):
     return render(request, 'tasks.html')
 
 def task(request, task_id):
-    return render(request, "task.html", {"task_id": task_id})
+    task = get_object_or_404(Task, pk=task_id)
+    context = {
+        "title": task.title,
+        "description": task.description,
+        "completed": task.completed,
+        "created_at": task.created_at
+    }
+    return render(request, "task.html", context)
