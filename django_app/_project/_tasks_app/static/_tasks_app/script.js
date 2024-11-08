a16f22b8bd2b06bcb6385727d7681dade502c85c
VaclavHrbek <st43190@student.upce.cz>
2024-11-08 22:13:49 +0100

    Add separate javascritp file.

---
+++ django_app/_project/_tasks_app/static/_tasks_app/script.js
@@ -0,0 +1,34 @@
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function updateTaskStatus(taskId, checkbox) {
+    const csrfToken = getCookie('csrftoken');
+    fetch(`/tasks/update_status/${taskId}/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrfToken
+        },
+        body: JSON.stringify({
+            'completed': checkbox.checked
+        })
+    }).then(response => {
+        if (!response.ok) {
+            alert('Failed to update task status.');
+        }
+    });
+}
