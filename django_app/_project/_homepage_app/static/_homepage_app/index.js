ff286963e94501dbcb62180d0666b661fca17b00
VaclavHrbek <st43190@student.upce.cz>
2024-11-13 17:56:17 +0100

    Update frontend.

--- django_app/_project/_homepage_app/static/_homepage_app/index.js
+++ django_app/_project/_homepage_app/static/_homepage_app/index.js
@@ -5,10 +5,17 @@ createApp({
   setup() {
     const message = ref('Hello Vue!')
     const button = ref('Hello button!')
+    const toDoButtonHelpMessage = ref('Go to the To Do app')
 
     return {
       message, 
       button
     }
-  }
+  },
+  template:
+  `
+  <div>
+  <p>[[ message ]]</p>
+  </div>
+  `
 }).mount('#app')
